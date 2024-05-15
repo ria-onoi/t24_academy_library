5f5e13ad81ef7d16cd1cc5f7b2d5c4ffa38fef74
ria-onoi.mt <ria.onoi@metateam.co.jp>
2024-05-15 10:28:31 +0900

    貸出編集、バリデーション機能追加

--- src/main/java/jp/co/metateam/library/model/RentalManageDto.java
+++ src/main/java/jp/co/metateam/library/model/RentalManageDto.java
@@ -1,14 +1,20 @@
 package jp.co.metateam.library.model;
 
 import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jp.co.metateam.library.values.RentalStatus;
 import lombok.Getter;
 import lombok.Setter;
+
+//追加したインポート
+import java.util.Optional;
 
 /**
  * 貸出管理DTO
@@ -45,4 +51,78 @@ public class RentalManageDto {
     private Stock stock;
 
     private Account account;
+
+    //バリデーションチェック
+    public Optional<String> validationStatus(Integer previousRentalStatus){
+        /*貸出待ち→貸出中かつ貸出予定日が現在の日付になっていない場合のエラー
+         *貸出待ち→返却済みの場合のエラー
+         */
+        //変更前のステータスが貸出待ち　かつ　変更後のステータスが変更されているかどうか
+        if (previousRentalStatus == RentalStatus.RENT_WAIT.getValue() && previousRentalStatus != this.status){
+
+            
+            
+            //変更後のステータスが返却済みの場合
+             if(this.status == RentalStatus.RETURNED.getValue()){
+
+                //「「貸出待ち」から「返却済み」にステータス変更できません」という旨のエラー
+                return Optional.of("「貸出待ち」から「返却済み」にステータス変更できません");
+            }
+
+        }
+
+        /*貸出中→返却済み以外のステータス変更の場合のエラー
+         */
+        //変更前のステータスが貸出中　かつ　変更後のステータスが変更されているかどうか
+        if(previousRentalStatus == RentalStatus.RENTAlING.getValue() && previousRentalStatus != this.status){
+
+            //変更後のステータスが返却済み以外かどうか
+            if(this.status != RentalStatus.RETURNED.getValue()){
+
+                //「「貸出中」から「返却済み」以外のステータスに変更できません」という旨のエラー
+                return Optional.of("「貸出中」から「返却済み」以外のステータスに変更できません");
+            }
+
+        }
+
+        /*返却済みからのステータス変更の場合のエラー
+         */
+        //変更前のステータスが返却済み　かつ　変更後のステータスが変更されているかどうか
+        if(previousRentalStatus == RentalStatus.RETURNED.getValue() && previousRentalStatus != this.status){
+
+            //「「返却済み」からステータスは変更できません」という旨のエラー
+            return Optional.of("「返却済み」からステータスは変更できません");
+        }
+
+        /*キャンセルからのステータス変更の場合のエラー
+         */
+        //変更前のステータスがキャンセル　かつ　変更後のステータスが変更されているかどうか
+        if(previousRentalStatus == RentalStatus.CANCELED.getValue() && previousRentalStatus != this.status){
+
+            //「「キャンセル」からステータスは変更できません」という旨のエラー
+            return Optional.of("「キャンセル」からステータスは変更できません");
+        }
+
+        // 上記の条件に該当しない場合は、デフォルトの空の Optional を返す
+        //つまり、エラーがない場合
+        return Optional.empty();
+
+    }
+
+    
+    //バリデーションチェック
+    public Optional<String> validationRentalOn(){
+
+      //変更後のステータスが貸出中だけど現在の日付ではない場合
+      if(this.status == RentalStatus.RENTAlING.getValue() && !Timestamp.valueOf(LocalDateTime.now()).equals(this.expectedRentalOn)){
+
+      //「現在の日付で入力してください」という旨のエラー
+      return Optional.of("現在の日付で入力してください");
+      }
+
+      return Optional.empty();
+        
+    }
+
+
 }
--- src/main/java/jp/co/metateam/library/service/RentalManageService.java
+++ src/main/java/jp/co/metateam/library/service/RentalManageService.java
@@ -8,9 +8,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import jp.co.metateam.library.model.Account;
+//import jp.co.metateam.library.model.BookMst;
 import jp.co.metateam.library.model.RentalManage;
 import jp.co.metateam.library.model.RentalManageDto;
 import jp.co.metateam.library.model.Stock;
+//import jp.co.metateam.library.model.StockDto;
 import jp.co.metateam.library.repository.AccountRepository;
 import jp.co.metateam.library.repository.RentalManageRepository;
 import jp.co.metateam.library.repository.StockRepository;
@@ -49,6 +51,13 @@ public class RentalManageService {
     @Transactional 
     public void save(RentalManageDto rentalManageDto) throws Exception {
         try {
+            //左辺：は名前を付けただけ
+            /*①RentalManageテーブルから直接EmployeeIdは取れないから画面上に入力した情報から社員番号情報を見つけなければならない
+            　
+              rentalManageDto.getEmployeeId()は画面上に入力した社員番号を引数
+            　orElse(null)は探したときに値が見つからないときは代わりにnullを代入してね
+              ifでnullだった時にはエラーが出るように書いている
+            */
             Account account = this.accountRepository.findByEmployeeId(rentalManageDto.getEmployeeId()).orElse(null);
             if (account == null) {
                 throw new Exception("Account not found.");
@@ -61,12 +70,48 @@ public class RentalManageService {
 
             RentalManage rentalManage = new RentalManage();
             rentalManage = setRentalStatusDate(rentalManage, rentalManageDto.getStatus());
-
+            
+            /*①上のAccount account = this.accountRepository.findByEmployeeId(rentalManageDto.getEmployeeId()).orElse(null);で
+              getしたaccountをデータベースであるrentalManageのsetAccountクラスに引数としてsetしている
+            */
             rentalManage.setAccount(account);
             rentalManage.setExpectedRentalOn(rentalManageDto.getExpectedRentalOn());
             rentalManage.setExpectedReturnOn(rentalManageDto.getExpectedReturnOn());
             rentalManage.setStatus(rentalManageDto.getStatus());
             rentalManage.setStock(stock);
+
+            // データベースへの保存
+            this.rentalManageRepository.save(rentalManage);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    @Transactional 
+    public void update(Long id, RentalManageDto rentalManageDto) throws Exception {
+        try {
+            Account account = this.accountRepository.findByEmployeeId(rentalManageDto.getEmployeeId()).orElse(null);
+            if (account == null) {
+                throw new Exception("Account not found.");
+            }
+
+            Stock stock = this.stockRepository.findById(rentalManageDto.getStockId()).orElse(null);
+            if (stock == null) {
+                throw new Exception("Stock not found.");
+            }
+
+            RentalManage rentalManage = this.rentalManageRepository.findById(id).orElse(null);
+            if (rentalManage == null) {
+                throw new Exception("RentalManage not found.");
+            }
+
+            rentalManage = setRentalStatusDate(rentalManage, rentalManageDto.getStatus());
+            rentalManage.setId(rentalManageDto.getId());
+            rentalManage.setAccount(account);
+            rentalManage.setExpectedRentalOn(rentalManageDto.getExpectedRentalOn());
+            rentalManage.setExpectedReturnOn(rentalManageDto.getExpectedReturnOn());
+            rentalManage.setStock(stock);
+            rentalManage.setStatus(rentalManageDto.getStatus());
 
             // データベースへの保存
             this.rentalManageRepository.save(rentalManage);
